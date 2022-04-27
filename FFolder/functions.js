7ed1f4b6f682de50873ebbf73712808be3ce826c
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2022-04-27 14:20:00 -0500

    Add files via upload

---
+++ FFolder/functions.js
@@ -0,0 +1,5 @@
+function writetext(){
+    text("Hello!", 200,150);
+}
+
+  
---
+++ sketch.js
@@ -0,0 +1,19 @@
+//create variables, or declarate; 
+
+var  a;
+
+
+function setup() {
+  createCanvas(600, 300);
+  //a= createSprite(200,200,20,20);
+  
+}
+
+function draw() {
+  background(220);
+  textSize(20);
+    fill(0,0,255);
+    writetext(); 
+}
+
+//creating functions 
